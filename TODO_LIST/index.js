79a6abb2e14df3eaca835fb8f19e1104607f4582
Suraj Patra <patrasuraj2000@gmail.com>
2023-08-25 21:31:31 +0530

    Completed by adding styles and the features

--- TODO_LIST/index.js
+++ TODO_LIST/index.js
@@ -5,13 +5,13 @@ let todo_arr = [
     { todo: 'When', isCompleted: false },
 ];
 
-// Selecting the form :
+// Selecting the 'form', 'ul', 'input' :
 const todo_form = document.querySelector('#todo-form');
-
-// Selecting the 'ul', 'input'
 const ul = document.getElementById('lists');
 const todo_input = document.getElementById('todo-input');
 
+
+/* --------------- Showing Todo list from the array  -------------------- */
 function show_todo(todo_arr) {
     ul.innerHTML = '';
     todo_arr.forEach(obj => {
@@ -19,9 +19,9 @@ function show_todo(todo_arr) {
         const li = document.createElement('li');
         li.classList.add('list');
         li.innerHTML = `
-            <button class='done'>C</button>
+            <button class='done'>&#10004;</button>
             <span>${obj.todo}</span>
-            <button class='remove'>X</button>
+            <button class='remove'>&#10006;</button>
         `
 
         obj.isCompleted?
@@ -35,10 +35,8 @@ function show_todo(todo_arr) {
 }
 show_todo(todo_arr);
 
-
-
     
-/* --------------- Adding list to the 'ul' -------------------- */
+/* --------------- Adding list to the array -------------------- */
 todo_form.addEventListener('submit', (e) => {
     e.preventDefault();     // preventing default submit behaviour
     
@@ -52,62 +50,25 @@ todo_form.addEventListener('submit', (e) => {
 
 /* ------ Completing and Removing lists with Event-delegation ---------- */
 ul.addEventListener('click', (e) => {
+    const li = e.target.parentElement;
+    const span = li.querySelector('span');
+    const content = span.textContent;
+    const index = todo_arr.findIndex(obj => obj.todo===content);
+
     if(e.target.classList.contains('done')) {   // if the class is 'done'
-        const li = e.target.parentElement;
-        const span = li.querySelector('span');
-        const content = span.textContent;
-        const index = todo_arr.findIndex(obj => obj.todo===content);
-        
-        todo_arr[index].isCompleted = !(todo_arr[index].isCompleted);
-        
-        show_todo(todo_arr)
+        // toggle 'isCompleted' :
+        todo_arr[index].isCompleted = !(todo_arr[index].isCompleted);        
+        // show the updated list :
+        show_todo(todo_arr);
     } else if(e.target.classList.contains('remove')) {  // if the class is 'remove'
-        const li = e.target.parentElement
-        li.remove();
+        // Remove the object with the content :
+        todo_arr.splice(index, 1);        
+        // show the updated list :
+        show_todo(todo_arr);
     }
 })
 
-
-function tasks_left() {
-    const span = document.querySelector('#tasks-left');
-    let count = 0;
-    todo_arr.forEach(obj => !obj.isCompleted && count++);
-    span.textContent = `${count} tasks left`;
-}
-tasks_left();
-
-
-
-function show_completed() {
-    const completed_list_arr = todo_arr.filter(obj => obj.isCompleted); 
-    show_todo(completed_list_arr);
-}
-const show_completed_btn = document.querySelector('#show-completed');
-show_completed_btn.addEventListener('click', () => {
-    show_completed();
-})
-
-
-function show_all() {
-    show_todo(todo_arr);
-}
-const show_all_btn = document.querySelector('#show-all');
-show_all_btn.addEventListener('click', () => {
-    show_all();
-})
-
-
-function show_uncompleted() {
-    const completed_list_arr = todo_arr.filter(obj => !obj.isCompleted); 
-    show_todo(completed_list_arr);
-}
-const show_uncompleted_btn = document.querySelector('#show-uncompleted');
-show_uncompleted_btn.addEventListener('click', () => {
-    show_uncompleted();
-})
-
-
-
+/* --------------- complete all tasks ---------------- */
 function complete_all() {
     todo_arr = todo_arr.map(obj => {
         return {...obj, isCompleted:true}
@@ -119,7 +80,7 @@ complete_all_btn.addEventListener('click', () => {
     complete_all();
 })
 
-
+/* ----------------- clear the completed tasks ------------ */
 function clear_completed() {
     todo_arr = todo_arr.filter(obj => !obj.isCompleted); 
     show_todo(todo_arr);
@@ -128,3 +89,49 @@ const clear_completed_btn = document.querySelector('#clear-completed');
 clear_completed_btn.addEventListener('click', () => {
     clear_completed();
 })
+
+
+/* ----------------- Tasks left ---------------- */
+function tasks_left() {
+    const span = document.querySelector('#tasks-left');
+    let count = 0;
+    todo_arr.forEach(obj => !obj.isCompleted && count++);
+    span.textContent = `${count} tasks left`;
+}
+tasks_left();
+
+
+/* ------------------- show completed task --------------- */
+function show_completed() {
+    const completed_list_arr = todo_arr.filter(obj => obj.isCompleted); 
+    show_todo(completed_list_arr);
+}
+const show_completed_btn = document.querySelector('#show-completed');
+show_completed_btn.addEventListener('click', () => {
+    show_completed();
+})
+
+
+/* -------------- show all tasks ------------------- */
+function show_all() {
+    show_todo(todo_arr);
+}
+const show_all_btn = document.querySelector('#show-all');
+show_all_btn.addEventListener('click', () => {
+    show_all();
+})
+
+
+/* --------------- show uncompleted tasks ---------------- */
+function show_uncompleted() {
+    const completed_list_arr = todo_arr.filter(obj => !obj.isCompleted); 
+    show_todo(completed_list_arr);
+}
+const show_uncompleted_btn = document.querySelector('#show-uncompleted');
+show_uncompleted_btn.addEventListener('click', () => {
+    show_uncompleted();
+})
+
+
+
+
